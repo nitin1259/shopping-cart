f3a9f9e8502a81f752374570391358376c0ef54d
Nitin SINGH <nitinkumar.singh@f5.com>
2020-09-18 09:28:29 +0530

    decoration product component as part 3

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -19,50 +19,50 @@ function HomePage() {
       <div className="home__row">
         <Product
           id="1234"
-          title="The Lean startup:How constant invocation creates"
+          title="Lenovo Ideapad Slim 3i 10th Gen Intel Core i3 14 inch FHD Thin and Light Laptop (4GB/256GB/Windows 10/MS Office/Grey/1.6Kg), 81WD0044IN"
           price={33.43}
           image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
           rating={5}
         />
         <Product
+          id="123412"
+          title="Samsung Galaxy M31 (Ocean Blue, 6GB RAM, 128GB Storage)"
+          price={23.43}
+          image="https://images-na.ssl-images-amazon.com/images/I/71-Su4Wr0HL._SL1500_.jpg"
+          rating={4}
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="123432"
+          title="OnePlus 8 Pro (Glacial Green 8GB RAM+128GB Storage)"
+          price={55.43}
+          image="https://images-na.ssl-images-amazon.com/images/I/61fv4VZRQ7L._SL1500_.jpg"
+          rating={3}
+        />
+        <Product
           id="1234"
-          title="The Lean startup:How constant invocation creates"
-          price={33.43}
-          image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
+          title="Logitech H390 USB Headset (Black)s"
+          price={13.43}
+          image="https://images-na.ssl-images-amazon.com/images/I/61CqYq%2BxwNL._SL1500_.jpg"
           rating={5}
+        />
+        <Product
+          id="1234"
+          title="The Storyteller: the heart-breaking and unforgettable novel by the number one bestselling"
+          price={7.12}
+          image="https://images-na.ssl-images-amazon.com/images/I/51GfKb1bfdL.jpg"
+          rating={2}
         />
       </div>
 
       <div className="home__row">
         <Product
           id="1234"
-          title="The Lean startup:How constant invocation creates"
-          price={33.43}
-          image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
-          rating={5}
-        />
-        <Product
-          id="1234"
-          title="The Lean startup:How constant invocation creates"
-          price={33.43}
-          image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
-          rating={5}
-        />
-        <Product
-          id="1234"
-          title="The Lean startup:How constant invocation creates"
-          price={33.43}
-          image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
-          rating={5}
-        />
-      </div>
-
-      <div className="home__row">
-        <Product
-          id="1234"
-          title="The Lean startup:How constant invocation creates"
-          price={33.43}
-          image="https://images-na.ssl-images-amazon.com/images/I/712rw0zcH8L._SL1500_.jpg"
+          title="Sony HT-S20R 5.1 Channel Dolby Digital Soundbar Home Theatre System with Bluetooth Connectivity - Black"
+          price={55.05}
+          image="https://images-na.ssl-images-amazon.com/images/I/71%2Bs6K1eovL._SL1500_.jpg"
           rating={5}
         />
       </div>
