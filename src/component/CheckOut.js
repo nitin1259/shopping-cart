339fb268e263319ab2b33f1d915914201ddf846c
Nitin SINGH <nitinkumar.singh@f5.com>
2020-09-19 18:38:18 +0530

    adding user in the checout cart page

--- src/component/CheckOut.js
+++ src/component/CheckOut.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function CheckOut() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -14,7 +14,7 @@ function CheckOut() {
           src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/CEPC/Wearables/Mi/Redmi_Band/SaleOn/MI-Xiaomi_redmi_Smart_Band_shop-now_1500x300.jpg"
           alt="offer image"
         />
-
+        <h3>Hi, {user?.email}</h3>
         {basket?.length === 0 ? (
           <div>
             <h2>Your Shopping Basket is empty </h2>
