ca94e654d89ee7925870c03174a6099a1140eade
Nitin SINGH <nitinkumar.singh@f5.com>
2020-09-28 16:17:28 +0530

    updated api url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/shopping-mart-5b0ea/us-central1/api", // api(cloud function) URL
+  // baseURL: "http://localhost:5001/shopping-mart-5b0ea/us-central1/api", // api(cloud function) URL
+  baseURL: "https://us-central1-shopping-mart-5b0ea.cloudfunctions.net/api",
 });
 
 export default instance;
